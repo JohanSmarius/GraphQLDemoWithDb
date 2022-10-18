aef381d705594d5e24617d5b56397ee6ffeaacb5
johansmarius <jsmarius@hotmail.com>
2022-10-18 11:07:02 +0200

    Added StrawberryShake client

---
+++ StrawberryShakeClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StrawberryShakeClient;
+using StrawberryShake;
+
+IServiceCollection serviceCollection = new ServiceCollection();
+serviceCollection.AddOrderClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:7189/graphql/"));
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var client = serviceProvider.GetRequiredService<IOrderClient>();
+
+var result = await client.Orders.ExecuteAsync(CancellationToken.None);
+
+if (!result.Errors.Any())
+{
+    foreach (var order in result!.Data!.Orders)
+    {
+        Console.WriteLine(order.Id);
+        foreach (var orderLine in order.OrderLines)
+        {
+            Console.WriteLine(orderLine.Product.Name);
+        }
+    }
+}
